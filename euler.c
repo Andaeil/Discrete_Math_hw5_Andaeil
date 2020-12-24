bd659e94b51ef545f32d2be81962b73c2aa80411
Andaeil <71754742+Andaeil@users.noreply.github.com>
2020-12-24 19:50:33 +0900

    Add files via upload

--- euler.c
+++ euler.c
@@ -4,15 +4,17 @@
 
 void taka_a_tour(graph_t * g, int start){
 	int curr = start;
-	int n_edges, next;
+	int next;
 
 	do{
 		printf("%d\n", curr);
 		for(next = 0; next < g->n_vertices; next++){
 			if(g->m[curr][next] > 0){
+				g->m[curr][next]--; 
 				break;
 			}
 		}
+		
 		if(next < g->n_vertices){
 			graph_remove_edge(g, curr, next, g->w[curr][next][0]);
 			curr = next;
@@ -29,7 +31,34 @@ main ()
 
 	g = graph_read_stdin() ;
 
-	take_a_tour(g, 0);
+	int start;
+	int odd_v = 0;
+
+	for(int i = 0; i < g->n_vertices; i++){
+		int count = 0;
+		for(int j = 0; j < g->n_vertices; j++){
+			count += g->m[i][j];
+		}
+		if(count%2 != 0){
+			odd_v += 1;
+		}
+	}
+
+	if(odd_v == 2){
+		for(int i = 0; i < g->n_vertices; i++){
+			int count = 0;
+			for(int j = 0; j < g->n_vertices; j++){
+				count += g->m[i][j];
+			}
+			if(count%2 != 0){
+				start = i;
+				break;
+			}
+		}
+		take_a_tour(g, start);
+	}else if(odd_v == 0){
+		take_a_tour(g, 0);
+	}
 
 	/* TODO: implement here */
 
